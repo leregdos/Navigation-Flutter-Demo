5991a960e74b11f278192c0f90818811ca66acab
Sodgerel Mandakhnaran <sm8694@nyu.edu>
2021-02-16 19:28:20 +0800

    Added routes

--- lib/screen0.dart
+++ lib/screen0.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'screen2.dart';
+import 'screen1.dart';
 
 class Screen0 extends StatelessWidget {
   @override
@@ -16,6 +18,7 @@ class Screen0 extends StatelessWidget {
               child: Text('Go To Screen 1'),
               onPressed: () {
                 //Navigate to Screen 1
+                Navigator.pushNamed(context, '/first');
               },
             ),
             RaisedButton(
@@ -23,6 +26,7 @@ class Screen0 extends StatelessWidget {
               child: Text('Go To Screen 2'),
               onPressed: () {
                 //Navigate to Screen 2
+                Navigator.pushNamed(context, '/first');
               },
             ),
           ],
